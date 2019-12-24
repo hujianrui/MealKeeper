5ee1ea494910eea3d15d5b3c882a0f89786e1a96
Jianrui Hu <u0886750@utah.edum>
2019-12-24 11:30:06 -0700

    recipe layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("test recipe", "test description", 
+    "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg")
+  ];
 
   constructor() { }
 
