ab6bfa57fb97821adf9baa149326114b1b9081e7
Jianrui Hu <u0886750@utah.edum>
2019-12-24 15:38:30 -0700

    shopping list layout

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,6 +1,4 @@
 export class Recipe {
-
     constructor(public name: string, 
-        public description: string, public imagePath: string){
-    }
+        public description: string, public imagePath: string){}
 }
