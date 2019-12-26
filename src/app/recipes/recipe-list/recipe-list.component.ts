9c082187379b75a5faef45f31c49dd4b649402cb
Jianrui Hu <u0886750@utah.edum>
2019-12-25 22:39:55 -0700

    recipe data binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>(); 
   recipes: Recipe[] = [
     new Recipe("test recipe", "test description", 
+    "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg"),
+    new Recipe("test recipe 2", "test description 2", 
     "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg")
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
