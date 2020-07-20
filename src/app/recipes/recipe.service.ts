6def621ea34e5251f89a184cfbc47922b2ccbd79
Jianrui Hu <u0886750@utah.edum>
2020-07-20 17:18:16 -0600

    link to firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,13 +6,19 @@ export class RecipeService{
     recipeChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
-        new Recipe("test recipe", "test description", 
-        "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg",
-        [new Ingredient('Meat', 1), new Ingredient('Tomato',2)]),
-        new Recipe("test recipe 2", "test description 2", 
-        "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg",
-        [new Ingredient('Chill', 1), new Ingredient('Potato',2)])
-      ];
+        // new Recipe("test recipe", "test description", 
+        // "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg",
+        // [new Ingredient('Meat', 1), new Ingredient('Tomato',2)]),
+        // new Recipe("test recipe 2", "test description 2", 
+        // "https://live.staticflickr.com/7254/7040034205_cb850610b5_b.jpg",
+        // [new Ingredient('Chill', 1), new Ingredient('Potato',2)])
+    ];
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
     getRecipes(): Recipe[]{
         return this.recipes.slice();
     }
